425c15ab91c204994529c50f79f92a83dc54b766
IQ72 <105931001+raihanfaiq72@users.noreply.github.com>
2024-06-19 18:38:53 +0700

    push

--- simple-app/app/Http/Controllers/PythonController.php
+++ simple-app/app/Http/Controllers/PythonController.php
@@ -8,20 +8,34 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class PythonController extends Controller
 {
+    // public function index()
+    // {
+    //     $tempat = base_path('kode3.py');
+
+    //     $proses = new Process([
+    //         'python3',$tempat
+    //     ]);
+
+    //     $proses->run();
+
+    //     if(!$proses->isSuccessful()){
+    //         throw new ProcessFailedException($proses);
+    //     }
+
+    //     $output = $proses->getOutput();
+
+    //     return response()->json([
+    //         'output'    => $output
+    //     ]);
+    // }
+
     public function index()
     {
-        $proses = new Process([
-            'python3',base_path('../kode3_yt.py')
-        ]);
-
-        if(!$proses->isSuccessful()){
-            throw new ProcessFailedException($proses);
-        }
-
-        $keluaran = $proses->getOutput();
+        // $output = shell_exec('python3'.base_path('/home/user/Project/comvis/simple-app/kode3.py'));
+        $output = shell_exec('python3'.base_path('/kode3.py'));
 
         return response()->json([
-            'keluaran'  => $keluaran
+            'output'    => $output
         ]);
     }
 }
